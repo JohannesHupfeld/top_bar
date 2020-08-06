03bc5bfb9006a41007f10bb77f84a31c1a747596
JohannesHupfeld <j.hupfeld@gmail.com>
2020-08-06 14:58:39 -0400

    completed environment

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,3 +1,16 @@
-require "top_bar/version"
+require 'pry'
+require 'open-uri'
+require 'nokogiri'
+require 'colorize'
+
+require_relative "top_bar/version"
+require_relative "top_bar/bar"
+require_relative "top_bar/cli"
+require_relative "top_bar/review"
+require_relative "top_bar/scraper"
 
 
+#module NetflixHorror
+#  class Error < StandardError; end
+#  # Your code goes here...
+#end
