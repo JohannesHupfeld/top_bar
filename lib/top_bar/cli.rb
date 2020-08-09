aff7b7ed58de1e8bcd6d80da5670c387add55b71
JohannesHupfeld <j.hupfeld@gmail.com>
2020-08-08 23:34:26 -0400

    list bars method

--- lib/top_bar/cli.rb
+++ lib/top_bar/cli.rb
@@ -1,8 +1,21 @@
 class TopBar::CLI
-
+    
     def start
-        puts "hello"
+        puts "Welcome to the Top Bar app!"
+        puts "Here is a list of the top 50 bars in the world" 
+        #scrape all the movie - call the scraper class
+        #list the top 100 bars
+        TopBar::Scraper.scrape_bars
+        list_bars
+        #ask for input
+        #call another method
+    end
+
+    def list_bars
+        TopBar::Bar.all.each.with_index(1) do |bar,index|
+            puts "#{index}. #{bar.name}"
+        end
     end
 
     
-end
+end 
