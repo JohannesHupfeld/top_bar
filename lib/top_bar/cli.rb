b0482a97fa72db9a52b8f01e60ac1a0308765f47
JohannesHupfeld <j.hupfeld@gmail.com>
2020-08-09 00:00:52 -0400

    created sort bars method

--- lib/top_bar/cli.rb
+++ lib/top_bar/cli.rb
@@ -1,9 +1,11 @@
 class TopBar::CLI
+    attr_accessor :sorted_bars
     
     def start
         puts "Welcome to the Top Bar app!"
         puts "Here is a list of the top 50 bars in the world" 
         TopBar::Scraper.scrape_bars
+        sort_bars
         #scrape all the movie - call the scraper class
         #list the top 100 bars
         list_bars
@@ -12,8 +14,12 @@ class TopBar::CLI
         #call another method
     end
 
+    def sort_bars
+        @sorted_bars = TopBar::Bar.all.sort_by{|bar| bar.name}
+    end
+
     def list_bars
-        TopBar::Bar.all.each.with_index(1) do |bar,index|
+        @sorted_bars.each.with_index(1) do |bar,index|
             puts "#{index}. #{bar.name}"
         end
     end
