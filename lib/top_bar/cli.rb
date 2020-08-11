48898d16a4d1db33049d4fe9a11097f8ca9520ae
JohannesHupfeld <j.hupfeld@gmail.com>
2020-08-11 09:29:35 -0400

    changed method name to choose bar

--- lib/top_bar/cli.rb
+++ lib/top_bar/cli.rb
@@ -1,5 +1,5 @@
 class TopBar::CLI
-    attr_accessor :sorted_bars
+    attr_accessor :sorted_bars #sorted bars as an attribute because we only really use it in the CLI class
     
     def start
         puts "Welcome to the Top Bar app!"
@@ -9,13 +9,14 @@ class TopBar::CLI
         #scrape all the movie - call the scraper class
         #list the top 100 bars
         list_bars
-        get_bar_method
+        puts "Please select the corresponding number (1-50) of the bar you would like more information on:"
+        choose_bar
         #ask for input
         #call another method
     end
 
     def sort_bars
-        @sorted_bars = TopBar::Bar.all.sort_by{|bar| bar.name}
+        @sorted_bars = TopBar::Bar.all.sort_by{|bar| bar.name}#parameters to sort by name alphabetically
     end
 
     def list_bars
@@ -24,8 +25,15 @@ class TopBar::CLI
         end
     end
 
-    def get_bar_method
-        puts "Please select a number (1-50) that corresponds to the bar you want more information on:"
+    def choose_bar
+        input = gets.strip
+        if input.to_i.between?(1,50) #between? is inclusive -thats why it starts at 1 not 0
+        else
+            puts "Invalid input...Please try again."
+            choose_bars# recursion - recalling a method inside a method starts this method over again
+        end 
+
+
     end
 
     
